997350e0717fe777afcf02748da97a605382dbd3
sparrro <116090214+sparrro@users.noreply.github.com>
2024-05-02 23:40:42 +0200

    final final for real

--- index.js
+++ index.js
@@ -6,6 +6,11 @@ const swaggerUi = require("swagger-ui-express");
 const swaggerDocs = require("./swagger-docs.json");
 
 
+const JWT_SECRET = process.env.JWT_SECRET || "1234"
+const BASE_URL = process.env.BASE_URL || "127.0.0.1"
+const PORT = process.env.PORT || "3000"
+
+
 app.use(express.json());
 
 app.use("/api/docs", swaggerUi.serve, swaggerUi.setup(swaggerDocs));
